efbd5f1e118007cee0293fa5453d334f60f13520
Heine HC <chris.outwright@gmail.com>
2020-01-13 14:14:06 +0100

    Percentage correct calculation fixed

--- src/neuralNet/Main.java
+++ src/neuralNet/Main.java
@@ -14,29 +14,32 @@ public class Main {
 		Datensatz training2 = new Datensatz("training2");
 		Random rnd = new Random();
 
-		for (int i = 0; i < 6000; i++) {
+		for (int i = 0; i < 600; i++) {
 
-			double x = Math.pow(rnd.nextDouble() * 2, 0.33) - 1;
-			double y = (rnd.nextDouble() * 2 - 1);
+			double x = Math.pow(rnd.nextDouble() * 2, 1) - 1;
+			double y = Math.pow(rnd.nextDouble() * 2, 1) - 1;
 
-			boolean tester = (y >= x * x * x);
+			boolean tester = y>x;
 
 			training2.addDatenpunkt(new Datenpunkt(x, y, tester));
 
 		}
 
-		int[] nodesInHiddenLayer = { 4,5,3 };
+		int[] nodesInHiddenLayer = { 5, 3 };
 
 		NeuralNet nn = new NeuralNet(nodesInHiddenLayer.length, nodesInHiddenLayer, training2);
 		nn.printAllLayerNodes();
-		int count = 0;
+		int[] count = { 0, 0 };
 		int count2 = 0;
 		int zahl = 100;
+		System.out.println("Layers: " +nodesInHiddenLayer.length);
+		int nlayers = nodesInHiddenLayer.length;
 
 		// System.out.println(nn.getNLayer(1).getLayerNeuronArray().get(3).getReceivers().size());
 
 		for (int j = 0; j < zahl; j++) {
-			count = 0;
+			count[0] = 0;
+			count[1] = 0;
 			count2 = 0;
 
 			double sum = 0.0;
@@ -55,9 +58,10 @@ public class Main {
 				sum += nn.giveError();
 
 				if (nn.getPointLabel()) {
-					if ((nn.getNLayer(2).getLayerNeuronArray().get(0).getOutput()) > (nn.getNLayer(2)
+					count[1]++;
+					if ((nn.getNLayer(nlayers).getLayerNeuronArray().get(0).getOutput()) > (nn.getNLayer(nlayers)
 							.getLayerNeuronArray().get(1).getOutput())) {
-						count++;
+						count[0]++;
 					} else { /*
 								 * System.out.println((nn.getPointLabel() ? 1 : 0) + " " + (nn.getPointLabel() ?
 								 * 0 : 1));
@@ -67,9 +71,9 @@ public class Main {
 
 					}
 				} else {
-					if ((nn.getNLayer(2).getLayerNeuronArray().get(1).getOutput()) > (nn.getNLayer(2)
+					if ((nn.getNLayer(nlayers).getLayerNeuronArray().get(1).getOutput()) > (nn.getNLayer(nlayers)
 							.getLayerNeuronArray().get(0).getOutput())) {
-						count++;
+						count[0]++;
 					} else { /*
 								 * System.out.println((nn.getPointLabel() ? 1 : 0) + " " + (nn.getPointLabel() ?
 								 * 0 : 1));
@@ -81,22 +85,28 @@ public class Main {
 				}
 
 				nn.updateAllLayersWeights();
-				
-				for(Neuron in : nn.getNLayer(1).getLayerNeuronArray().get(0).getInputs().keySet()) {
-				
-					
-				/*	if(in instanceof BiasNeuron) {
-					System.out.println("Weight Bias in H1 to H2 N1: ");
-					System.out.println(nn.getNLayer(1).getLayerNeuronArray().get(0).getInputs().get(in));
-					System.out.println(nn.getNLayer(1).getLayerNeuronArray().get(1).getInputs().get(in));
-					
-					
-					
-					
-				
-				}*/
-					
-				}
+
+				/*
+				 * for (Neuron in :
+				 * nn.getNLayer(1).getLayerNeuronArray().get(0).getInputs().keySet()) {
+				 * 
+				 * 
+				 * if(in instanceof BiasNeuron) {
+				 * System.out.println("Weight Bias in H1 to H2 N1: ");
+				 * System.out.println(nn.getNLayer(1).getLayerNeuronArray().get(0).getInputs().
+				 * get(in));
+				 * System.out.println(nn.getNLayer(1).getLayerNeuronArray().get(1).getInputs().
+				 * get(in));
+				 * 
+				 * 
+				 * 
+				 * 
+				 * 
+				 * }
+				 * 
+				 * 
+				 * }
+				 */
 
 				count2 = i;
 				nn.nextPoint();
@@ -106,18 +116,20 @@ public class Main {
 
 			System.out.println("Epoch " + j + " finished");
 			// System.out.println(count);
-
-			System.out.println(((float) count) / training2.getList().size());
+			System.out.print("Percentage correct: ");
+			System.out.println(((float) count[0]) / training2.getList().size() + "\n");
+			System.out.print("Percentage True Labels to All Label: ");
+			System.out.println(((float) count[1]) / training2.getList().size() + "\n");
 			System.out.println(training2.getList().size());
 
 			// training2.removeAllPoints();
 
-			for (int i = 0; i < 10000; i++) {
+			for (int i = 0; i < 1000; i++) {
 
-				double x = Math.pow(rnd.nextDouble() * 2, 0.33) - 1;
-				double y = (rnd.nextDouble() * 2 - 1);
+				double x = Math.pow(rnd.nextDouble() * 2, 1) - 1;
+				double y = Math.pow(rnd.nextDouble() * 2, 1) - 1;
 
-				boolean tester = (y >= x * x * x);
+				boolean tester = y>x;
 
 				training2.addDatenpunkt(new Datenpunkt(x, y, tester));
 
